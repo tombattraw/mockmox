372c432c4a28ea4d98b13399d800d1d32b3adc3d
tom <tom.battraw@gmail.com>
2025-07-07 22:05:29 -0400

    Added VM template class and worked on configuration/installation

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 A VM management framework intended to abstract away most details of qemu/KVM VM management. I tried Proxmox but was endlessly annoyed by the layers of GUI management and dislike the CLI, so I'm building my own, since, as we all know, building projects in-house is *always* superior to a tested and widely-accepted third-party solution. 
+
+Sadly, not all requirements can be satisfied with just PIP. If you're running mockmox on the same system, install all below packages on it.
+Otherwise, install the "client" packages on the system with the script, and the "server" packages on the system running libvirtd.
+
+Server:
+Arch: virt-manager
+Debian/Ubuntu/Mint: virtinst python3-virtinst
+RHEL/CentOS/Fedora: virt-install python3-virtinst
--- mockmox.py
+++ mockmox.py
@@ -1,24 +1,117 @@
 import os
-import subprocess
 import argparse
 import pathlib
+import libvirt
+from virtinst import cli
 import yaml
 import sys
 import shutil
 import subprocess
+import logging
 
 
 BASE_DIR = pathlib.Path("/opt/mockmox")
+CONFIG_FILE = pathlib.Path("/etc/mockmox/config.yaml")
+
 GROUP_DIR = BASE_DIR / "groups"
-VM_DIR = BASE_DIR / "vms"
+VM_TEMPLATE_DIR = BASE_DIR / "vms"
 ACTIVE_DIR = BASE_DIR / "active"
 SUSPENDED_DIR = BASE_DIR / "suspended"
+DEFAULT_DIR = BASE_DIR / "defaults"
 
 SCRIPT_LOCATION = pathlib.Path("/bin/mockmox")
 
 VM_DEFAULT_DISK_SIZE = 64   # given in GB
 VM_DEFAULT_CPUS = 4
 VM_DEFAULT_MEMORY = 8192    # given in MB
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(message)s"
+)
+
+
+class VMTemplate:
+    def __init__(self, name: str, action: str,
+                 os_variant: str = '',
+                 disk_size: int = VM_DEFAULT_DISK_SIZE,
+                 cpus: int = VM_DEFAULT_CPUS,
+                 memory: int = VM_DEFAULT_MEMORY,
+                 existing_disk_image: pathlib.Path = None,
+                 iso: pathlib.Path = None):
+        self.name = name
+        self.logger = logging.getLogger(f"{__name__}.{self.__class__.__name__}")
+        self.path = VM_TEMPLATE_DIR / name
+        self.disk = self.path / f"{self.name}.qcow2"
+        self.user_script_dir = self.path / "user_scripts"
+        self.root_script_dir = self.path / "root_scripts"
+        self.files_dir = self.path / "files"
+        self.config = self.path / f"{self.name}_config.yaml"
+
+        match action:
+            case "load":
+                if not (self.path.exists() and self.disk.exists()):
+                    raise FileNotFoundError(f"VM template {self.name} is missing critical components and cannot start. Repair or delete and recreate the template.")
+
+                # Scripts and files aren't mandatory
+                self.user_scripts = self.user_script_dir.iterdir() if self.user_script_dir.exists() else []
+                self.root_scripts = self.root_script_dir.iterdir() if self.root_script_dir.exists() else []
+                self.files = self.files_dir.iterdir() if self.files_dir.exists() else []
+
+            case "delete":
+                if not self.path.exists():
+                    raise FileNotFoundError(f"VM template {self.name} does not exist.")
+
+                shutil.rmtree(self.path)
+
+            case 'create':
+                if self.path.exists():
+                    raise FileExistsError(f"VM template {self.name} already exists. Delete it first to recreate it.")
+
+                if not (iso or existing_disk_image):
+                    raise ValueError("An ISO or existing disk image is required when creating a VM")
+
+                if iso:
+                    if not iso.exists():
+                        raise FileNotFoundError(f"ISO {iso} does not exist.")
+                if existing_disk_image:
+                    if not existing_disk_image.exists():
+                        raise FileNotFoundError(f"Disk iamge {existing_disk_image} does not exist.")
+
+                self.path.mkdir()
+                self.user_script_dir.mkdir()
+                self.root_script_dir.mkdir()
+                self.files_dir.mkdir()
+
+                if not existing_disk_image:
+                    subprocess.run(f"qemu-img create -f qcow2 {self.disk} {disk_size}", capture_output=False)
+                    subprocess.run(f"virt-install --name {name} --vcpus {cpus} --memory {memory} --os-variant {os_variant} --controller=scsi,model=virtio-scsi --disk path={self.disk},bus=scsi --cdrom={iso} --noreboot")
+                else:
+                    shutil.copyfile(existing_disk_image, self.disk)
+
+
+
+
+
+
+class Group:
+    def __init__(self, name: str):
+        self.name = name
+        self.logger = logging.getLogger(f"{__name__}.{self.__class__.__name__}")
+        self.vm_templates = []
+        self.path = GROUP_DIR / name
+
+        if self.path.exists():
+            self.vm_templates = GROUP_DIR
+
+
+
+
+def load_state():
+    pass
+
+
 
 
 def get_editor():
@@ -61,6 +154,16 @@ if __name__ == "__main__":
         prog="mockmox.py",
         description="VM Management Framework")
 
+    if CONFIG_FILE.exists():
+        config = yaml.safe_load(CONFIG_FILE.read_text())
+    else:
+        config = {}
+
+    arg_def.add_argument("-l", "--libvirtd",
+                         default=config.get("libvirtd") if config.get("libvirtd") else "qemu:///system",
+                         required=False,
+                         help=f"The libvirtd hypervisor to connect to. Default is in the config file \"{CONFIG_FILE}\"")
+
     # Groups are logical collections of VMs, down to just one
     # VMs are only used as templates; they must be added to groups to start
     # Groups are started, suspended, and stopped together
@@ -80,8 +183,8 @@ if __name__ == "__main__":
                            help="Number of CPUs (default: %(default)s)")
     vm_create.add_argument("-m", "--memory", type=int, default=VM_DEFAULT_MEMORY,
                            help="Memory size in MB (default: %(default)s)")
-    vm_create.add_argument("--existing-qcow2", help="Use existing disk image")
-    vm_create.add_argument("-i", "--iso", help="Path to installation ISO")
+    vm_create.add_argument("--existing-qcow2", help="Use existing disk image", type=pathlib.Path, required=False)
+    vm_create.add_argument("-i", "--iso", help="Path to installation ISO", type=pathlib.Path, required=False)
 
     vm_delete = vm_sp.add_parser("delete")
     vm_delete.add_argument("name", help="Name of VM template to delete")
@@ -117,7 +220,7 @@ if __name__ == "__main__":
 
     group_resume = group_sp.add_parser("resume")
     group_resume.add_argument("name", help="Group to resume")
-    
+
     group_edit = group_sp.add_parser("edit", help="Edit a group's configuration")
     group_edit.add_argument("name", help="Name of group to edit")
 
